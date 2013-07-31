2d68e9eb108cc88ba43a3e3785e349082e4b9831
Chris van Hasselt <cvanhasselt@mindspring.com>
2013-07-31 16:28:40 -0400

    First code commit
    This first upload included JavaScript code, a test file, a data file,
    and copies of the version of qunit used for testing.

---
+++ js/geode.js
@@ -0,0 +1,193 @@
+   var $Z = {};
+  /* 
+   * GeoDE: a JavaScript based Geographic Data Engine.
+   *
+   * Licensed under The MIT License (MIT)
+   *
+   * Copyright (c) 2013 Christopher Gerard van Hasselt
+   *
+   * Permission is hereby granted, free of charge, to any person obtaining a copy
+   * of this software and associated documentation files (the "Software"), to deal
+   * in the Software without restriction, including without limitation the rights
+   * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   * copies of the Software, and to permit persons to whom the Software is
+   * furnished to do so, subject to the following conditions:
+
+   * The above copyright notice and this permission notice shall be included in
+   * all copies or substantial portions of the Software.
+
+   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   * THE SOFTWARE.
+   *
+   ********************************************************************************
+   */
+    
+    // isArray -- Mozilla code found here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+    // needed for the deep array searching.
+    Array.isArray = Array.isArray || function (vArg) {
+        return Object.prototype.toString.call(vArg) === "[object Array]";
+    };
+      
+    /*
+     * GeoDE is the JavaScript Geographic Data Engine.  It is designed to bring 
+     *   geographic metadata, beginning with ISO-3166 country codes, into a JavaScript
+     *   client environment.  
+     *
+     *  @author     Chris van Hasselt 
+     *  
+     *  @param      {string}    source  source is the name of the JSON source file where country data is stored.
+     *                                  The default source file is isodata.json.
+     */
+
+    var GeoDE = GeoDE || function (source) {
+
+        var self           = this,        // reference to current object
+            dataLoaded     = false,       // private variable, used to note whether data is loaded; read access through isDataLoaded() method
+            dataFile       = source,      // private variable, used to store name of the GeoDE object datafile.  
+                                          // Multiple GeoDE objects could be used with different datafiles
+            recentSearches = [];
+
+        /* function setIsoData assigns data returned from a JSON file, named as the dataFile variable,
+         * to the isoData property Object.
+         *
+         */
+        function setIsodata(xhrResponse) {
+            self.isodata = JSON.parse(xhrResponse);
+            return true;
+        };
+        
+        /*
+         * function getCountryIndex finds the country index from any list, implemented as an array.
+         *    For individual list items, the function performs a deep search of an array if the item is an array. 
+         *    See test cases for examples.  This function is private, but reachable via getCountryInfo()
+         *    
+         *    @param    {Array}   list    Any of the (so far 4) possible country information lists
+         *    @param    {string}  query   A value sought from the country list 
+         *    @return   {number}          Integer index of the array element found
+         */
+        function getCountryIndex(list, query) {
+        
+          var counter     = list.length - 1,        // used to count down through the list passed in that will be searched
+              deepcounter = 0,                      // deepcounter is used when the list item is an array, rather than a single value
+              qLower      = query.toLowerCase(),    // the lowercase value of the item being searched for.  All searches are lowercased.
+              deepfound   = false,                  // true if a match is found within a deeper level of the item.
+              keepgoing   = true;                   // keepgoing until match is found.
+
+          do {
+            // The list item may be either a single value, or an array.  if it is an array, a search within this array is needed.
+            // The only list that may contain arrays is the isonames list, which, for example, has the following entry
+            //
+            //         [
+            //            "Holy See (Vatican City State)",
+            //            "Vatican",
+            //            "Vatican City",
+            //            "Holy See"
+            //         ]
+            //
+            if (Array.isArray(list[counter])) {
+              deepcounter = list[counter].length - 1;
+              do {
+                if (qLower === list[counter][deepcounter].toLowerCase()) {
+                  deepfound = true; 
+                  keepgoing = false; 
+                };
+              } while (!deepfound && deepcounter--);
+            } else {
+                if (qLower === list[counter].toLowerCase()) { 
+                    keepgoing = false;
+                }
+            }
+          } while (keepgoing && counter--);
+
+          return counter;
+        };
+        
+        /*
+         * function search 
+         *    
+         *    @param    {Array}   list    Any of the (so far 4) possible country information lists
+         *    @param    {string}  query   A value sought from the country list 
+         *    @return   {number}          Integer index of the array element found
+         */
+        function search(term) {
+
+          var currIdx = 0; 
+
+          // get the index of the searchTerm, if available 
+          if (term.match(/^[a-zA-Z]{2}$/i) !== null) {
+            // search term is iso3166-alpha2 code
+            currIdx = getCountryIndex(self.isodata.alpha2, term);
+          } else if (term.match(/^[a-zA-Z]{3}$/i)) {
+            // search term is an iso3166-alpha3 code
+            currIdx = getCountryIndex(self.isodata.alpha3, term);
+          } else if (term.match(/^\d{3}$/i)) {
+            // search term is a UN-M49 code
+            currIdx = getCountryIndex(self.isodata.unm49, term);
+          } else {
+            // search term is a country name
+            currIdx = getCountryIndex(self.isodata.isonames, term);
+          };
+          return currIdx;
+        };
+        
+        /*
+         *  public function to load data.  The loadData() function should be called after the object is created.
+         *      The data will be loaded from the dataFile variable.  The callback function is called
+         * 
+         *    @param    {Function}  callback    callback is a callback, ran after data is loaded.  In general, all code on a
+         *                                      page that will use the geographic data should begin via this callback.
+         */
+        this.loadData = function (callback) {
+          var xhr         = new XMLHttpRequest();
+
+          xhr.onreadystatechange = function () {
+              if (xhr.readyState === 4 && xhr.status === 200) {
+                dataLoaded = setIsodata(xhr.responseText);
+                callback();
+              };
+          };
+          xhr.open("GET", dataFile, true);
+          xhr.send(null);
+        };
+        
+        /*
+         * getCountryInfo
+         *    
+         *    @param    {string}  searchTerm    Any of the (so far 4) possible country information lists
+         *    @return   {number}                Integer index of the array element found
+         */
+        this.getCountryInfo = function (searchTerm) { 
+      
+            var idx = 0,
+                s   = '';
+
+            if (typeof (searchTerm) === "number") {
+                s = (Math.floor(Math.abs(searchTerm)) + 1000).toString().substring(1);
+            } else {
+                s = searchTerm;
+            };
+            idx = search(s);
+
+            // create a country info object, and add it to the list of recent searches.
+            
+            
+            // return country info object
+            return {
+                query      : searchTerm,
+                name       : (Array.isArray(self.isodata.isonames[idx])) ? self.isodata.isonames[idx][0] : self.isodata.isonames[idx],
+                alpha2     : self.isodata.alpha2[idx],
+                alpha3     : self.isodata.alpha3[idx],
+                unm49      : self.isodata.unm49[idx]
+            };
+        };
+ 
+        this.isDataLoaded = function () {
+            return dataLoaded;
+        };
+
+      };
---
+++ tests/GeoDETests.js
@@ -0,0 +1,114 @@
+    module('Module 1');
+        asyncTest('Testing if data is loaded', 1, function() {
+            // check to see if data is loaded
+            setupGeode(function () {
+                equal($Z.isDataLoaded(), true, 'Data is Loaded');
+                start();
+            }); 
+        });
+
+        
+    module('Module 2');
+        test('Testing data returns', 28, function() {
+
+            var country          = 'Afghanistan',
+                countryAllCaps   = 'AFGHANISTAN',
+                countryAlpha2    = 'AF',
+                countryAlpha3    = 'AFG',
+                unm49Code        = '004',
+                unm49Number      = 4,
+                Zcountry         = 'Zimbabwe',
+                ZcountryAllCaps  = 'ZIMBABWE',
+                ZcountryAlpha2   = 'ZW',
+                ZcountryAlpha3   = 'ZWE',
+                Zunm49Code       = '716',
+                Zunm49Number     = 716;
+                
+            // test with country name, Afghanistan
+            equal($Z.getCountryInfo(country).alpha2, 'AF','Expected value to be \'AF\'');
+            equal($Z.getCountryInfo(country).alpha3, 'AFG','Expected value to be \'AFG\'');
+            equal($Z.getCountryInfo(country).name, 'Afghanistan','Expected value to be \'Afghanistan\'');
+            equal($Z.getCountryInfo(country).unm49, '004','Expected value to be \'004\'');
+
+            // test return with all caps country name, AFGHANISTAN
+            equal($Z.getCountryInfo(countryAllCaps).alpha2, 'AF','Expected value to be \'AF\'');
+            equal($Z.getCountryInfo(countryAllCaps).alpha3, 'AFG','Expected value to be \'AFG\'');
+            equal($Z.getCountryInfo(countryAllCaps).name, 'Afghanistan','Expected value to be \'Afghanistan\'');
+            equal($Z.getCountryInfo(countryAllCaps).unm49, '004','Expected value to be \'004\'');
+
+            // test return for searching by alpha2 country code, AF
+            equal($Z.getCountryInfo(countryAlpha2).alpha2, 'AF','Expected value to be \'AF\'');
+            equal($Z.getCountryInfo(countryAlpha2).alpha3, 'AFG','Expected value to be \'AFG\'');
+            equal($Z.getCountryInfo(countryAlpha2).name, 'Afghanistan','Expected value to be \'Afghanistan\'');
+            equal($Z.getCountryInfo(countryAlpha2).unm49, '004','Expected value to be \'004\'');
+
+            // test return for searching for alpha3 country code, AFG
+            equal($Z.getCountryInfo(countryAlpha3).alpha2, 'AF','Expected value to be \'AF\' testing with \'' + countryAlpha3);
+            equal($Z.getCountryInfo(countryAlpha3).alpha3, 'AFG','Expected value to be \'AFG\'');
+            equal($Z.getCountryInfo(countryAlpha3).name, 'Afghanistan','Expected value to be \'Afghanistan\'');
+            equal($Z.getCountryInfo(countryAlpha3).unm49, '004','Expected value to be \'004\'');
+
+            // test return for searching for UN M.49 country code, 004
+            equal($Z.getCountryInfo(unm49Code).alpha2, 'AF','Expected value to be \'AF\' testing with \'' + unm49Code );
+            equal($Z.getCountryInfo(unm49Code).alpha3, 'AFG','Expected value to be \'AFG\'');
+            equal($Z.getCountryInfo(unm49Code).name, 'Afghanistan','Expected value to be \'Afghanistan\'');
+            equal($Z.getCountryInfo(unm49Code).unm49, '004','Expected value to be \'004\'');
+
+            // test return for searching for UN M.49 country code as a number, 4
+            equal($Z.getCountryInfo(unm49Number).alpha2, 'AF','Expected value to be \'AF\'');
+            equal($Z.getCountryInfo(unm49Number).alpha3, 'AFG','Expected value to be \'AFG\'');
+            equal($Z.getCountryInfo(unm49Number).name, 'Afghanistan','Expected value to be \'Afghanistan\'');
+            equal($Z.getCountryInfo(unm49Number).unm49, '004','Expected value to be \'004\'');  
+            
+            // test with country name, Zimbabwe
+            equal($Z.getCountryInfo(Zcountry).alpha2, 'ZW','Expected value to be \'ZW\'');
+            equal($Z.getCountryInfo(Zcountry).alpha3, 'ZWE','Expected value to be \'ZWE\'');
+            equal($Z.getCountryInfo(Zcountry).name, 'Zimbabwe','Expected value to be \'Zimbabwe\'');
+            equal($Z.getCountryInfo(Zcountry).unm49, '716','Expected value to be \'716\'');
+      }); 
+
+    module('Module 3');
+        test('Testing for a few countries with alternate names.', 2, function() {
+        
+            var country1        = 'Austria',
+                altCountry1     = 'Austria, Republic of',
+                country2        = 'Vatican',
+                altCountry2     = 'Holy See';
+   
+            equal($Z.getCountryInfo(country1).alpha2, $Z.getCountryInfo(altCountry1).alpha2, '\'' + country1 + '\' should return same value as \'' + altCountry1 +'\'' );
+            equal($Z.getCountryInfo(country2).alpha2, $Z.getCountryInfo(altCountry2).alpha2, '\'' + country2 + '\' should return same value as \'' + altCountry2 +'\'' );
+        });
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
